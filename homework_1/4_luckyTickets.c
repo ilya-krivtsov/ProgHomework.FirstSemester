125ffae939cc90d696f591e69d4f6dcb89482f0b
ilya-krivtsov <180809461+ilya-krivtsov@users.noreply.github.com>
2024-09-11 05:50:44 +0300

    Fixed task 4

--- homework_1/4_luckyTickets.c
+++ homework_1/4_luckyTickets.c
@@ -1,17 +1,21 @@
 #include <stdio.h>
 
-int main()
+int countTickets()
 {
-    int sums[28], count = 0;
+    int sums[28] = {0}, count = 0;
 
-    for (int i = 0; i < 28; i++)
-        sums[i] = 0;
-
-    for (int i = 0; i < 1000; i++)
-        sums[((i / 100) % 10) + ((i / 10) % 10) + (i % 10)]++;
+    for (int i = 0; i < 10; i++)
+        for (int j = 0; j < 10; j++)
+            for (int k = 0; k < 10; k++)
+                sums[i + j + k]++;
 
     for (int i = 0; i < 28; i++)
         count += sums[i] * sums[i];
 
-    printf("Total count of lucky tickets is %d\n", count);
+    return count;
+}
+
+int main()
+{
+    printf("Total count of lucky tickets is %d\n", countTickets());
 }
