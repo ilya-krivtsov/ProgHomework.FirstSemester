805ff4982eb14f5ce0421991e54c68fe6547632d
ilya-krivtsov <180809461+ilya-krivtsov@users.noreply.github.com>
2024-10-08 01:25:10 +0300

    Added comments to sorting functions (hw2 task 2)

--- homework_2/task_2/sortings.h
+++ homework_2/task_2/sortings.h
@@ -4,5 +4,12 @@
 #define VALUE_MAX 4095
 #define VALUES_COUNT (VALUE_MAX - VALUE_MIN)
 
+/// @brief Sorts array using counting algorithm
+/// @param array Array to sort
+/// @param arrayLength Length of `array`
 void countingSort(int *array, int arrayLength);
+
+/// @brief Sorts array with bubble sort
+/// @param array Array to sort
+/// @param arrayLength Length of `array`
 void bubbleSort(int *array, int arrayLength);
