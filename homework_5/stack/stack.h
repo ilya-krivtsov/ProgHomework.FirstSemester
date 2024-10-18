5851423bd4836f981b186c9cd5f9b637f9c4fd4f
ilya-krivtsov <180809461+ilya-krivtsov@users.noreply.github.com>
2024-10-19 00:35:21 +0300

    Homework 5 - Added stack

---
+++ homework_5/stack/stack.h
@@ -0,0 +1,38 @@
+#pragma once
+
+#include <stdbool.h>
+#include <stdint.h>
+
+/// @brief Stack that stores all of its elements as `uint64_t`
+typedef struct Stack Stack;
+
+/// @brief Creates `Stack`
+/// @param stack Pointer to store created `Stack` to
+/// @return `true` if successful, `false` otherwise (allocation failed)
+bool stackCreate(Stack **stack);
+
+/// @brief Pushes value on top of the stack
+/// @param stack Pointer to `Stack` instance
+/// @return `true` if successful, `false` otherwise (allocation failed)
+bool stackPush(Stack *stack, uint64_t value);
+
+/// @brief Pops value from the top of the stack
+/// @param stack Pointer to `Stack` instance 
+/// @param value Pointer to write value to 
+/// @return `true` if stack was non-empty, `false` otherwise
+bool stackTryPop(Stack *stack, uint64_t *value);
+
+/// @brief Returns value from the top of the stack without removing it
+/// @param stack Pointer to `Stack` instance
+/// @param value Pointer to write value to 
+/// @return `true` if stack was non-empty, `false` otherwise
+bool stackTryPeek(Stack *stack, uint64_t *value);
+
+/// @brief Checks if stack is empty
+/// @param stack Pointer to `Stack` instance
+/// @return `true` if stack is empty, `false` otherwise
+bool stackIsEmpty(Stack *stack);
+
+/// @brief Disposes stack and all of its elements
+/// @param stack `Stack` to dispose 
+void stackDispose(Stack *stack);
---
+++ homework_5/stack/test.c
@@ -0,0 +1,147 @@
+#define CTEST_MAIN
+#define CTEST_SEGFAULT
+#include "../../ctest/ctest.h"
+
+#include <stdint.h>
+
+#include "stack.h"
+
+int main(int argc, const char *argv[]) {
+    return ctest_main(argc, argv);
+}
+
+#pragma region Helpers
+
+Stack *createStack(void) {
+    Stack *stack;
+    ASSERT_TRUE(stackCreate(&stack));
+    ASSERT_NOT_NULL(stack);
+
+    return stack;
+}
+
+void assertPop(Stack *stack, uint64_t value) {
+    uint64_t top = 0;
+    ASSERT_TRUE(stackTryPop(stack, &top));
+    ASSERT_EQUAL_U(value, top);
+}
+
+void assertPeek(Stack *stack, uint64_t value) {
+    uint64_t top = 0;
+    ASSERT_TRUE(stackTryPeek(stack, &top));
+    ASSERT_EQUAL_U(value, top);
+}
+
+#pragma endregion
+
+CTEST(stackTests, createTest) {
+    Stack *stack = createStack();
+    ASSERT_TRUE(stackIsEmpty(stack));
+    free(stack);
+}
+
+#pragma region Push & Pop
+
+CTEST(stackTests, pushPopTestA) {
+    Stack *stack = createStack();
+
+    ASSERT_TRUE(stackPush(stack, 0));
+    assertPop(stack, 0);
+
+    ASSERT_TRUE(stackIsEmpty(stack));
+
+    free(stack);
+}
+
+CTEST(stackTests, pushPopTestB) {
+    Stack *stack = createStack();
+
+    ASSERT_TRUE(stackPush(stack, 0));
+    assertPop(stack, 0);
+    ASSERT_TRUE(stackIsEmpty(stack));
+
+    ASSERT_TRUE(stackPush(stack, 47));
+    assertPop(stack, 47);
+    ASSERT_TRUE(stackIsEmpty(stack));
+
+    ASSERT_TRUE(stackPush(stack, 1245));
+    ASSERT_TRUE(stackPush(stack, 44362));
+    assertPop(stack, 44362);
+    assertPop(stack, 1245);
+    ASSERT_TRUE(stackIsEmpty(stack));
+
+    free(stack);
+}
+
+CTEST(stackTests, pushPopTestC) {
+    Stack *stack = createStack();
+
+    ASSERT_TRUE(stackPush(stack, 2352));
+    ASSERT_TRUE(stackPush(stack, 43634));
+    assertPop(stack, 43634);
+    ASSERT_FALSE(stackIsEmpty(stack));
+
+    ASSERT_TRUE(stackPush(stack, 9999));
+    ASSERT_TRUE(stackPush(stack, 7777777));
+    assertPop(stack, 7777777);
+    assertPop(stack, 9999);
+    assertPop(stack, 2352);
+    ASSERT_TRUE(stackIsEmpty(stack));
+
+    free(stack);
+}
+
+#pragma endregion
+
+#pragma region Push & Peek
+
+CTEST(stackTests, pushPeekTestA) {
+    Stack *stack = createStack();
+
+    ASSERT_TRUE(stackPush(stack, 0));
+    assertPeek(stack, 0);
+
+    ASSERT_FALSE(stackIsEmpty(stack));
+
+    free(stack);
+}
+
+CTEST(stackTests, pushPeekTestB) {
+    Stack *stack = createStack();
+
+    ASSERT_TRUE(stackPush(stack, 0));
+    assertPeek(stack, 0);
+    ASSERT_FALSE(stackIsEmpty(stack));
+
+    ASSERT_TRUE(stackPush(stack, 47));
+    assertPeek(stack, 47);
+    ASSERT_FALSE(stackIsEmpty(stack));
+
+    ASSERT_TRUE(stackPush(stack, 1245));
+    ASSERT_TRUE(stackPush(stack, 44362));
+    assertPeek(stack, 44362);
+    assertPeek(stack, 44362);
+    ASSERT_FALSE(stackIsEmpty(stack));
+
+    free(stack);
+}
+
+CTEST(stackTests, pushPeekTestC) {
+    Stack *stack = createStack();
+
+    ASSERT_TRUE(stackPush(stack, 2352));
+    ASSERT_TRUE(stackPush(stack, 43634));
+    assertPeek(stack, 43634);
+    ASSERT_FALSE(stackIsEmpty(stack));
+
+    ASSERT_TRUE(stackPush(stack, 9999));
+    ASSERT_TRUE(stackPush(stack, 7777777));
+    assertPeek(stack, 7777777);
+    assertPeek(stack, 7777777);
+    assertPeek(stack, 7777777);
+    ASSERT_FALSE(stackIsEmpty(stack));
+
+    free(stack);
+}
+
+#pragma endregion
