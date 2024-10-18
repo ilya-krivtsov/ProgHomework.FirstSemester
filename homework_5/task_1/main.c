7d6d6c39cbde9583fcbfae37b63e9d071f47bd53
ilya-krivtsov <180809461+ilya-krivtsov@users.noreply.github.com>
2024-10-19 01:54:25 +0300

    Homework 5 task 1

---
+++ homework_5/task_1/main.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+#include "bracketsBalance.h"
+
+int main(void) {
+    printf("input string: ");
+    char buffer[1024] = { 0 };
+
+    if (scanf("%1023s") != 1) {
+        printf("reading string failed\n");
+        return 1;
+    }
+
+    switch (checkIfBracketsBalanced(buffer))
+    {
+    case BALANCED:
+        printf("brackets are balanced\n");
+        break;
+
+    case UNBALANCED:
+        printf("brackets are unbalanced\n");
+        break;
+
+    case ALLOCATION_FAILED:
+        printf("allocation failed\n");
+        return 1;
+    }
+}
