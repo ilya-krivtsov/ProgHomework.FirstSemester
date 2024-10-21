3796939272b779a539bbc4249072593e711cead1
ilya-krivtsov <180809461+ilya-krivtsov@users.noreply.github.com>
2024-10-21 16:54:48 +0300

    Homework 5 - task 3

---
+++ homework_5/task_3/shuntingYard.h
@@ -0,0 +1,19 @@
+#pragma once
+
+typedef struct ShuntingYard ShuntingYard;
+
+typedef enum {
+    YARD_OK,
+    YARD_UNKNOWN_INPUT,
+    YARD_INCORRECT_INPUT,
+    YARD_BUFFER_OVERFLOW,
+    YARD_ALLOCATION_ERROR
+} YardConsumeResult;
+
+typedef void (*OutputFunction)(char output);
+
+bool shuntingYardCreate(ShuntingYard **yard);
+
+YardConsumeResult shuntingYardConsumeInput(ShuntingYard *yard, char input, char *outputBuffer, int bufferLength, int *bufferPosition);
+
+void shuntingYardDispose(ShuntingYard *yard);
