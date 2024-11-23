a27eb062667ba863bb51b4f77d88de2026bfa74e
ilya-krivtsov <180809461+ilya-krivtsov@users.noreply.github.com>
2024-11-23 21:52:13 +0300

    Added warning suppression (hw 2 tasks 3 and 4)

--- homework_2/task_3/main.c
+++ homework_2/task_3/main.c
@@ -12,11 +12,23 @@ int readValue(const char *prompt, const char *incorrectValueMessage) {
     return value;
 }
 
+// for some reason migw requires %llu format specifier for uint64_t, and gcc requires %lu,
+// so either in CI or on local machine with mingw there would be a format warning,
+// because of that %lu will be used here for uint64_t, and when compiling with mingw just suppress format warnings
+#ifdef __MINGW64__
+#pragma GCC diagnostic push
+#pragma GCC diagnostic ignored "-Wformat"
+#endif
+
 int main(void) {
     int base = readValue("enter base: ", "incorrect value: base cannot be less than zero; try again: ");
     int exponent = readValue("enter exponent: ", "incorrect value: exponent cannot be less than zero; try again: ");
 
-    printf("%d ^ %d = %llu (recursive: O(log n))\n", base, exponent, powerRecursive(base, exponent));
-    printf("%d ^ %d = %llu (iterative: O(log n))\n", base, exponent, powerIterative(base, exponent));
-    printf("%d ^ %d = %llu (naive: O(n))\n", base, exponent, powerNaive(base, exponent));
+    printf("%d ^ %d = %lu (recursive: O(log n))\n", base, exponent, powerRecursive(base, exponent));
+    printf("%d ^ %d = %lu (iterative: O(log n))\n", base, exponent, powerIterative(base, exponent));
+    printf("%d ^ %d = %lu (naive: O(n))\n", base, exponent, powerNaive(base, exponent));
 }
+
+#ifdef __MINGW64__
+// #pragma GCC diagnostic pop
+#endif
