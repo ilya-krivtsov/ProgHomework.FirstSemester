5a7931caaa1ef987e8f72f3b2e829d56a976e913
ilya-krivtsov <180809461+ilya-krivtsov@users.noreply.github.com>
2024-11-25 18:02:12 +0300

    Added hashtable expansion and iterator (hw9 task 1)

--- homework_9/task_1/frequencyLookup.c
+++ homework_9/task_1/frequencyLookup.c
@@ -2,6 +2,7 @@
 
 #include <stdbool.h>
 #include <stdlib.h>
+#include <stdio.h>
 #include <string.h>
 
 #include "frequencyList.h"
@@ -9,16 +10,24 @@
 typedef struct FrequencyLookup {
     FrequencyList **buckets;
     int capacity;
+    int count;
 } FrequencyLookup;
 
-bool createLookup(FrequencyLookup **lookup) {
+typedef struct LookupIterator {
+    FrequencyLookup *lookup;
+    int bucketIndex;
+    FrequencyElement *currentElement;
+} LookupIterator;
+
+static bool createLookupInternal(FrequencyLookup **lookup, int capacity) {
     FrequencyLookup *newLookup = malloc(sizeof(FrequencyLookup));
 
     if (newLookup == NULL) {
         return false;
     }
 
-    newLookup->capacity = 64;
+    newLookup->count = 0;
+    newLookup->capacity = capacity;
     newLookup->buckets = malloc(sizeof(FrequencyList *) * newLookup->capacity);
 
     bool failed = false;
@@ -38,6 +47,10 @@ bool createLookup(FrequencyLookup **lookup) {
     return true;
 }
 
+bool createLookup(FrequencyLookup **lookup) {
+    return createLookupInternal(lookup, 64);
+}
+
 static unsigned int getStringHash(const char *string) {
     unsigned int hash = 0;
     for (int i = 0; string[i] != '\0'; ++i) {
@@ -46,8 +59,61 @@ static unsigned int getStringHash(const char *string) {
     return hash;
 }
 
+static void disposeBuckets(FrequencyLookup *lookup) {
+    for (int i = 0; i < lookup->capacity; ++i) {
+        disposeList(lookup->buckets[i]);
+    }
+}
+
+static bool expandLookup(FrequencyLookup *lookup) {
+    int minBucketLength = getMinBucketLength(lookup);
+    int maxBucketLength = getMaxBucketLength(lookup);
+    printf("before expanding:\n");
+    printf("old: [capacity: %d, min length: %d, max length: %d]\n", lookup->capacity, minBucketLength, maxBucketLength);
+    printf("old [occup: %lf]\n", (float)lookup->count / lookup->capacity);
+
+    int newCapacity = lookup->capacity * 4;
+
+    FrequencyLookup *newLookup = NULL;
+    if (!createLookupInternal(&newLookup, newCapacity)) {
+        return false;
+    }
+
+    LookupIterator *iterator = getIterator(lookup);
+    while (moveNext(iterator)) {
+        const char *string = NULL;
+        int frequency = 0;
+        getCurrent(iterator, &string, &frequency);
+
+        if (!addFrequency(newLookup, string, frequency)) {
+            return false;
+        }
+    }
+    disposeIterator(iterator);
+
+    disposeBuckets(lookup);
+
+    // copy new lookup to old one
+    *lookup = *newLookup;
+
+    // free(), because disposeLookup() will dispose buckets
+    free(newLookup);
+
+    minBucketLength = getMinBucketLength(lookup);
+    maxBucketLength = getMaxBucketLength(lookup);
+    printf("expanded lookup;\n");
+    printf("new: [capacity: %d, min length: %d, max length: %d]\n", lookup->capacity, minBucketLength, maxBucketLength);
+    printf("new [occup: %lf]\n", (float)lookup->count / lookup->capacity);
+
+    return true;
+}
+
 // adds or creates new element
 bool addFrequency(FrequencyLookup *lookup, const char *string, int frequency) {
+    if ((float)lookup->count / lookup->capacity > 4.0) {
+        expandLookup(lookup);
+    }
+
     int bucketIndex = getStringHash(string) % lookup->capacity;
     FrequencyList *bucket = lookup->buckets[bucketIndex];
     FrequencyElement *element = getFirstElement(bucket);
@@ -65,6 +131,8 @@ bool addFrequency(FrequencyLookup *lookup, const char *string, int frequency) {
     if (element == NULL) {
         return false;
     }
+
+    ++lookup->count;
     setString(element, string);
     setFrequency(element, frequency);
     return true;
@@ -87,9 +155,73 @@ bool getFrequencyByString(FrequencyLookup *lookup, const char *string, int *freq
     return false;
 }
 
-void disposeLookup(FrequencyLookup *lookup) {
-    for (int i = 0; i < lookup->capacity; ++i) {
-        disposeList(lookup->buckets[i]);
+LookupIterator *getIterator(FrequencyLookup *lookup) {
+    LookupIterator *iterator = malloc(sizeof(LookupIterator));
+    if (iterator == NULL) {
+        return NULL;
     }
+
+    iterator->lookup = lookup;
+    iterator->bucketIndex = -1;
+    iterator->currentElement = NULL;
+
+    return iterator;
+}
+
+int getCount(FrequencyLookup *lookup) {
+    return lookup->count;
+}
+
+int getCapacity(FrequencyLookup *lookup) {
+    return lookup->capacity;
+}
+
+int getMinBucketLength(FrequencyLookup *lookup) {
+    int length = -1;
+    for (int i = 0; i < lookup->capacity; ++i) {
+        int bucketLength = getLength(lookup->buckets[i]);
+        if (length == -1 || bucketLength < length) {
+            length = bucketLength;
+        }
+    }
+    return length;
+}
+
+int getMaxBucketLength(FrequencyLookup *lookup) {
+    int length = -1;
+    for (int i = 0; i < lookup->capacity; ++i) {
+        int bucketLength = getLength(lookup->buckets[i]);
+        if (length == -1 || bucketLength > length) {
+            length = bucketLength;
+        }
+    }
+    return length;
+}
+
+void disposeLookup(FrequencyLookup *lookup) {
+    disposeBuckets(lookup);
     free(lookup);
+}
+
+bool moveNext(LookupIterator *iterator) {
+    while (iterator->currentElement == NULL) {
+        ++iterator->bucketIndex;
+        if (iterator->bucketIndex >= iterator->lookup->capacity) {
+            return false;
+        }
+        iterator->currentElement = getFirstElement(iterator->lookup->buckets[iterator->bucketIndex]);
+    }
+
+    return true;
+}
+
+void getCurrent(LookupIterator *iterator, const char **string, int *frequency) {
+    *string = getString(iterator->currentElement);
+    *frequency = getFrequency(iterator->currentElement);
+
+    iterator->currentElement = getNextElement(iterator->currentElement);
+}
+
+void disposeIterator(LookupIterator *iterator) {
+    free(iterator);
 }
--- homework_9/task_1/frequencyLookup.h
+++ homework_9/task_1/frequencyLookup.h
@@ -4,10 +4,28 @@
 
 typedef struct FrequencyLookup FrequencyLookup;
 
+typedef struct LookupIterator LookupIterator;
+
 bool createLookup(FrequencyLookup **lookup);
 
 bool addFrequency(FrequencyLookup *lookup, const char *string, int frequency);
 
 bool getFrequencyByString(FrequencyLookup *lookup, const char *string, int *frequency);
 
+LookupIterator *getIterator(FrequencyLookup *lookup);
+
+int getCount(FrequencyLookup *lookup);
+
+int getCapacity(FrequencyLookup *lookup);
+
+int getMinBucketLength(FrequencyLookup *lookup);
+
+int getMaxBucketLength(FrequencyLookup *lookup);
+
 void disposeLookup(FrequencyLookup *lookup);
+
+bool moveNext(LookupIterator *iterator);
+
+void getCurrent(LookupIterator *iterator, const char **string, int *frequency);
+
+void disposeIterator(LookupIterator *iterator);
