6a8ac657a1bd549dba705230041ed347743404f2
ilya-krivtsov <180809461+ilya-krivtsov@users.noreply.github.com>
2024-11-29 14:35:07 +0300

    Pass buffer as parameter to scanf (hw5 task 1)

--- homework_5/task_1/main.c
+++ homework_5/task_1/main.c
@@ -6,7 +6,7 @@ int main(void) {
     printf("input string: ");
     char buffer[1024] = { 0 };
 
-    if (scanf("%1023s") != 1) {
+    if (scanf("%1023s", buffer) != 1) {
         printf("reading string failed\n");
         return 1;
     }
