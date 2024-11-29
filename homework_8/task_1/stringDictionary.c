b87f8b40300db6d12f7a50ad53b41bff608c63a0
ilya-krivtsov <180809461+ilya-krivtsov@users.noreply.github.com>
2024-11-29 03:47:00 +0300

    Homework 8 - task 1

---
+++ homework_8/task_1/stringDictionary.c
@@ -0,0 +1,360 @@
+#include "stringDictionary.h"
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+typedef struct Node {
+    char *key;
+    char *value;
+    int balance;
+    struct Node *left;
+    struct Node *right;
+} Node;
+
+typedef struct StringDictionary {
+    Node *root;
+} StringDictionary;
+
+bool createDictionary(StringDictionary **dictionary) {
+    *dictionary = malloc(sizeof(StringDictionary));
+    if (*dictionary == NULL) {
+        return false;
+    }
+
+    (*dictionary)->root = NULL;
+
+    return true;
+}
+
+static bool createNode(Node **node, const char *key, const char *value) {
+    *node = malloc(sizeof(Node));
+    if (*node == NULL) {
+        return false;
+    }
+
+    char *keyCopy = strdup(key);
+    if (keyCopy == NULL) {
+        return false;
+    }
+    (*node)->key = keyCopy;
+
+    char *valueCopy = strdup(value);
+    if (valueCopy == NULL) {
+        return false;
+    }
+    (*node)->value = valueCopy;
+
+    (*node)->balance = 0;
+    (*node)->left = NULL;
+    (*node)->right = NULL;
+
+    return true;
+}
+
+static void disposeSingleNode(Node *node) {
+    free(node->key);
+    free(node->value);
+    free(node);
+}
+
+static Node *rotateLeft(Node *node) {
+    Node *right = node->right;
+    node->right = right->left;
+    right->left = node;
+
+    return right;
+}
+
+static Node *rotateRight(Node *node) {
+    Node *left = node->left;
+    node->left = left->right;
+    left->right = node;
+
+    return left;
+}
+
+static Node *rotateLeftMajor(Node *node) {
+    node->right = rotateRight(node->right);
+    return rotateLeft(node);
+}
+
+static Node *rotateRightMajor(Node *node) {
+    node->left = rotateLeft(node->left);
+    return rotateRight(node);
+}
+
+static Node *balanceNode(Node *node) {
+    if (node->balance == -2) {
+        if (node->right->balance <= 0) {
+            Node *newNode = rotateLeft(node);
+            if (newNode->balance == -1) {
+                node->balance = 0;
+                newNode->balance = 0;
+            } else {
+                node->balance = -1;
+                newNode->balance = 1;
+            }
+
+            return newNode;
+        }
+
+        Node *newNode = rotateLeftMajor(node);
+
+        newNode->left->balance = newNode->balance == -1 ? 1 : 0;
+        newNode->right->balance = newNode->balance == 1 ? -1 : 0;
+        newNode->balance = 0;
+
+        return newNode;
+    }
+
+    if (node->balance == 2) {
+        if (node->left->balance >= 0) {
+            Node *newNode = rotateRight(node);
+            if (newNode->balance == 1) {
+                node->balance = 0;
+                newNode->balance = 0;
+            } else {
+                node->balance = 1;
+                newNode->balance = -1;
+            }
+
+            return newNode;
+        }
+
+        Node *newNode = rotateRightMajor(node);
+
+        newNode->left->balance = newNode->balance == 1 ? -1 : 0;
+        newNode->right->balance = newNode->balance == -1 ? 1 : 0;
+        newNode->balance = 0;
+
+        return newNode;
+    }
+
+    return node;
+}
+
+int getHeight(Node *node) {
+    if (node == NULL) {
+        return 0;
+    }
+
+    int left = getHeight(node->left);
+    int right = getHeight(node->right);
+
+    return (left > right ? left : right) + 1;
+}
+
+static Node *insertNode(Node *node, const char *key, const char *value, bool *continueAscending, bool *created) {
+    if (node == NULL) {
+        Node *newNode = NULL;
+        *created = createNode(&newNode, key, value);
+
+        if (!(*created)) {
+            *continueAscending = false;
+        }
+
+        return newNode;
+    }
+
+    int cmp = strcmp(key, node->key);
+
+    if (cmp < 0) {
+        node->left = insertNode(node->left, key, value, continueAscending, created);
+        if (!(*continueAscending)) {
+            return node;
+        }
+
+        ++node->balance;
+    } else if (cmp > 0) {
+        node->right = insertNode(node->right, key, value, continueAscending, created);
+        if (!(*continueAscending)) {
+            return node;
+        }
+
+        --node->balance;
+    } else {
+        *continueAscending = false;
+
+        free(node->value);
+
+        char *valueCopy = strdup(value);
+        if (valueCopy == NULL) {
+            *created = false;
+        } else {
+            node->value = valueCopy;
+        }
+
+        return node;
+    }
+
+    // TODO: remove this
+    node->balance = (getHeight(node->left) - getHeight(node->right));
+
+    if (node->balance == 0) {
+        *continueAscending = false;
+        return node;
+    }
+
+    if (abs(node->balance) == 1) {
+        return node;
+    }
+
+    Node *balanced = balanceNode(node);
+
+    if (balanced->balance == 0) {
+        *continueAscending = false;
+    }
+
+    return balanced;
+}
+
+bool addToDictionary(StringDictionary *dictionary, const char *key, const char *value) {
+    bool continueAscending = true;
+    bool created = true;
+
+    Node *newRoot = insertNode(dictionary->root, key, value, &continueAscending, &created);
+    dictionary->root = newRoot;
+
+    return created;
+}
+
+Node *findNode(Node *node, const char *key) {
+    if (node == NULL) {
+        return NULL;
+    }
+
+    int cmp = strcmp(key, node->key);
+
+    if (cmp < 0) {
+        return findNode(node->left, key);
+    }
+
+    if (cmp > 0) {
+        return findNode(node->right, key);
+    }
+
+    return node;
+}
+
+const char *getValue(StringDictionary *dictionary, const char *key) {
+    Node *node = findNode(dictionary->root, key);
+
+    return node == NULL ? NULL : node->value;
+}
+
+bool containsKey(StringDictionary *dictionary, const char *key) {
+    return findNode(dictionary->root, key) != NULL;
+}
+
+static void swap(char **left, char **right) {
+    char *temp = *left;
+    *left = *right;
+    *right = temp;
+}
+
+static Node *deleteNode(Node *node, const char *key, bool *continueAscending, bool *found) {
+    if (node == NULL) {
+        *continueAscending = false;
+        *found = false;
+        return NULL;
+    }
+
+    int cmp = strcmp(key, node->key);
+
+    if (cmp < 0) {
+        node->left = deleteNode(node->left, key, continueAscending, found);
+
+        if (!(*continueAscending)) {
+            return node;
+        }
+
+        --node->balance;
+    } else if (cmp > 0) {
+        node->right = deleteNode(node->right, key, continueAscending, found);
+
+        if (!(*continueAscending)) {
+            return node;
+        }
+
+        ++node->balance;
+    } else {
+        if (node->left == NULL && node->right == NULL) {
+            disposeSingleNode(node);
+            return NULL;
+        }
+
+        if (node->left == NULL) {
+            Node *right = node->right;
+            disposeSingleNode(node);
+            return right;
+        }
+
+        if (node->right == NULL) {
+            Node *left = node->left;
+            disposeSingleNode(node);
+            return left;
+        }
+
+        Node *newNode = node->left;
+        while (newNode->right != NULL) {
+            newNode = newNode->right;
+        }
+
+        swap(&node->key, &newNode->key);
+        swap(&node->value, &newNode->value);
+
+        node->left = deleteNode(node->left, key, continueAscending, found);
+
+        if (!(*continueAscending)) {
+            return node;
+        }
+
+        --node->balance;
+    }
+
+    // TODO: remove this
+    node->balance = (getHeight(node->left) - getHeight(node->right));
+
+    if (abs(node->balance) == 1) {
+        *continueAscending = false;
+        return node;
+    }
+
+    if (node->balance == 0) {
+        return node;
+    }
+
+    Node *balanced = balanceNode(node);
+
+    if (abs(balanced->balance) == 1) {
+        *continueAscending = false;
+    }
+
+    return balanced;
+}
+
+bool removeFromDictionary(StringDictionary *dictionary, const char *key) {
+    bool continueAscending = true;
+    bool found = true;
+
+    dictionary->root = deleteNode(dictionary->root, key, &continueAscending, &found);
+
+    return found;
+}
+
+void disposeNodeRecursive(Node *node) {
+    if (node == NULL) {
+        return;
+    }
+
+    disposeNodeRecursive(node->left);
+    disposeNodeRecursive(node->right);
+
+    disposeSingleNode(node);
+}
+
+void disposeDictionary(StringDictionary *dictionary) {
+    disposeNodeRecursive(dictionary->root);
+    free(dictionary);
+}
---
+++ homework_8/task_1/test.c
@@ -0,0 +1,138 @@
+#define CTEST_MAIN
+#define CTEST_SEGFAULT
+#include "../../ctest/ctest.h"
+
+#include <stdio.h>
+
+#include "stringDictionary.h"
+
+int main(int argc, const char *argv[]) {
+    return ctest_main(argc, argv);
+}
+
+StringDictionary *createDict(void) {
+    StringDictionary *dictionary = NULL;
+    ASSERT_TRUE(createDictionary(&dictionary));
+    return dictionary;
+}
+
+void addToDict(StringDictionary *dict, const char *key, const char *value) {
+    ASSERT_TRUE(addToDictionary(dict, key, value));
+}
+
+CTEST(dictonaryTests, emptyTest) {
+    StringDictionary *dict = createDict();
+    disposeDictionary(dict);
+}
+
+CTEST(dictonaryTests, singleEntryTest) {
+    StringDictionary *dict = createDict();
+
+    const char *key = "key_0";
+    const char *value = "value_0";
+    addToDict(dict, key, value);
+    ASSERT_TRUE(containsKey(dict, key));
+
+    ASSERT_STR(getValue(dict, key), value);
+
+    ASSERT_TRUE(removeFromDictionary(dict, key));
+    ASSERT_FALSE(removeFromDictionary(dict, key));
+    ASSERT_FALSE(containsKey(dict, key));
+    ASSERT_NULL(getValue(dict, key));
+
+    disposeDictionary(dict);
+}
+
+void assertAddingAndRemoving(const char **keys, const char **values, int size) {
+    StringDictionary *dict = createDict();
+
+    for (int i = 0; i < size; ++i) {
+        addToDict(dict, keys[i], values[i]);
+        ASSERT_TRUE(containsKey(dict, keys[i]));
+        ASSERT_STR(getValue(dict, keys[i]), values[i]);
+    }
+
+    for (int i = 0; i < size; ++i) {
+        // ASSERT_EQUAL(getCount(dict), size);
+        ASSERT_TRUE(containsKey(dict, keys[i]));
+        ASSERT_STR(getValue(dict, keys[i]), values[i]);
+    }
+
+    for (int i = 0; i < size; ++i) {
+
+        int index = (i + (size / 3)) % size;
+        ASSERT_TRUE(removeFromDictionary(dict, keys[index]));
+        ASSERT_FALSE(removeFromDictionary(dict, keys[index]));
+        ASSERT_FALSE(containsKey(dict, keys[index]));
+        ASSERT_NULL(getValue(dict, keys[index]));
+    }
+
+    disposeDictionary(dict);
+}
+
+CTEST(dictonaryTests, binaryEntriesTest) {
+    const char *keys[15] = {
+        "key_32", "key_16", "key_48", "key_8", "key_24", "key_40", "key_56", "key_4",
+        "key_12", "key_20", "key_28", "key_36", "key_44", "key_52", "key_60"
+    };
+
+    const char *values[15] = {
+        "value_32", "value_16", "value_48", "value_8", "value_24", "value_40", "value_56", "value_4",
+        "value_12", "value_20", "value_28", "value_36", "value_44", "value_52", "value_60"
+    };
+
+    assertAddingAndRemoving(keys, values, 15);
+}
+
+CTEST(dictonaryTests, randomEntriesTest) {
+#define size 16384
+    char *keys[size] = { 0 };
+    char *values[size] = { 0 };
+
+    const char *keyTemplate = "key_000000";
+    const char *valueTemplate = "value_000000";
+    const int keyOffset = strlen("key_");
+    const int valueOffset = strlen("value_");
+    const int digitsCount = strlen(keyTemplate) - keyOffset;
+
+    for (int i = 0; i < size; ++i) {
+        char *key = strdup(keyTemplate);
+        char *value = strdup(valueTemplate);
+
+        ASSERT_NOT_NULL(key);
+        ASSERT_NOT_NULL(value);
+
+        keys[i] = key;
+        values[i] = value;
+
+        int number = i;
+        for (int j = digitsCount - 1; j >= 0; --j) {
+            char digit = (number % 10) + '0';
+            key[keyOffset + j] = digit;
+            value[valueOffset + j] = digit;
+            number /= 10;
+        }
+    }
+
+    srand(453563458);
+
+    for (int i = 0; i < size; ++i) {
+        int index = (rand() % (size - i)) + i;
+
+        char *key = keys[i];
+        keys[i] = keys[index];
+        keys[index] = key;
+
+        char *value = values[i];
+        values[i] = values[index];
+        values[index] = value;
+    }
+
+    assertAddingAndRemoving((const char **)keys, (const char **)values, size);
+
+    for (int i = 0; i < size; ++i) {
+        free(keys[i]);
+        free(values[i]);
+    }
+#undef size
+}
