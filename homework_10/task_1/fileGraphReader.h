10528b068615ee017033d3e227a866794890799c
ilya-krivtsov <180809461+ilya-krivtsov@users.noreply.github.com>
2024-12-04 22:25:27 +0300

    Moved reading graph from file and country creation to separate file (hw10 task 1)

---
+++ homework_10/task_1/fileGraphReader.h
@@ -0,0 +1,13 @@
+#pragma once
+
+#include <stdio.h>
+
+#include "graph.h"
+
+typedef enum {
+    READ_OK,
+    READ_ALLOCATION_ERROR,
+    READ_FILE_ERROR,
+} ReadResult;
+
+ReadResult readFileFromFileAndCreateCountries(FILE *file, GraphNode ***nodes, int *nodeCount, Country ***countries, int *countryCount, FILE *warningAndErrorOutput);
